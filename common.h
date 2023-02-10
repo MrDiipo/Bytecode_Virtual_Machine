6935635ad8e996e7877804b0c689d6e97e616e63
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2023-02-10 08:59:01 +0100

    Create bytecode representation

---
+++ common.h
@@ -0,0 +1,9 @@
+
+#ifndef BYTECODE_VIRTUAL_MACHINE_COMMON_H
+#define BYTECODE_VIRTUAL_MACHINE_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //BYTECODE_VIRTUAL_MACHINE_COMMON_H
