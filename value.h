f9628073f084c5dabb7d87812f88f8d53a690214
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2023-02-17 09:36:47 +0100

    Develop debugger

--- value.h
+++ value.h
@@ -2,4 +2,19 @@
 #ifndef BYTECODE_VIRTUAL_MACHINE_VALUE_H
 #define BYTECODE_VIRTUAL_MACHINE_VALUE_H
 
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
 #endif //BYTECODE_VIRTUAL_MACHINE_VALUE_H
