dc67b3254dade13505bf83aacd7b99d4a05410c0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2023-03-24 18:50:51 +0100

    Develop compiler

--- compiler.h
+++ compiler.h
@@ -2,6 +2,8 @@
 #ifndef BYTECODE_VIRTUAL_MACHINE_COMPILER_H
 #define BYTECODE_VIRTUAL_MACHINE_COMPILER_H
 
-void compile(const char* source);
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif //BYTECODE_VIRTUAL_MACHINE_COMPILER_H
